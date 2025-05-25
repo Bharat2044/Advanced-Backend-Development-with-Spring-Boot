5fe4bde4c265f7764a7c62009ace633c0678c59b
Bharat Kumar <bk9955204456@gmail.com>
2025-05-25 16:05:08 +0530

    code added

---
+++ 001. Basics of JAVA Programming Language/src/_11_Functions_in_JAVA/_07_Scoping.java
@@ -0,0 +1,23 @@
+package _11_Functions_in_JAVA;
+
+public class _07_Scoping {
+
+    private static void swap(int one, int two) {
+        System.out.println(one + ", " + two);
+
+        int temp = one;
+        one = two;
+        two = temp;
+
+        System.out.println(one + ", " + two);
+    }
+
+    public static void main(String[] args) {
+
+        int one = 10, two = 20;
+
+        System.out.println(one + ", " + two);
+        swap(one, two);
+        System.out.println(one + ", " + two);
+    }
+}
