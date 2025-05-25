a53e8e8cec6271856fa0bdaf2fe781b42c874d5c
Bharat Kumar <bk9955204456@gmail.com>
2025-05-25 16:02:57 +0530

    code added

---
+++ 001. Basics of JAVA Programming Language/src/_01_Basic_Programming_with_JAVA/_03_Variables.java
@@ -0,0 +1,19 @@
+package _01_Basic_Programming_with_JAVA;
+
+public class _03_Variables {
+
+    public static void main(String[] args) {
+
+        // Assigning value during declaration
+        String name = "Bharat"; // Here 'name' is a variables
+
+        // Declaring a variable
+        int age; // Here 'age' is a variables
+
+        // Assigning value to variable
+        age = 22;
+
+        System.out.println("Name = " + name);
+        System.out.println("Age = " + age);
+    }
+}
---
+++ 001. Basics of JAVA Programming Language/src/_02_Scanner_class_and_Operators_in_JAVA/_04_RelationalOperators.java
@@ -0,0 +1,18 @@
+package _02_Scanner_class_and_Operators_in_JAVA;
+
+public class _04_RelationalOperators {
+
+    public static void main(String[] args) {
+
+        int a = 8, b = 20;
+
+        System.out.println(a + " " + b);
+
+        System.out.println(a == b);
+        System.out.println(a != b);
+        System.out.println(a > b);
+        System.out.println(a < b);
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+    }
+}
