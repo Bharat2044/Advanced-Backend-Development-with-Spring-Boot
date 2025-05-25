f77e70e152179a4da9b2f72f965500e2841b380f
Bharat Kumar <bk9955204456@gmail.com>
2025-05-25 17:02:34 +0530

    code added

---
+++ 002. What is build system actually - Introduction to Bazel/02_SampleBazelProject/app/com/example/main/Main.java
@@ -0,0 +1,14 @@
+package com.example.main;
+
+import com.example.lib.Greeting;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello from the Main class!");
+
+        Greeting.sayHello();
+
+        Test.test();
+    }
+}
