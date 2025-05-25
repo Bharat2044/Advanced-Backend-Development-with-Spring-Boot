ef505576efaa5df8de8f32ee63454c0780b2dbbe
Bharat Kumar <bk9955204456@gmail.com>
2025-05-25 16:02:31 +0530

    code added

---
+++ 001. Basics of JAVA Programming Language/src/_01_Basic_Programming_with_JAVA/_02_Outputs.java
@@ -0,0 +1,24 @@
+package _01_Basic_Programming_with_JAVA;
+
+public class _02_Outputs {
+
+    public static void main(String[] args) {
+
+        System.out.println("1. println");
+        System.out.println("2. println");
+
+        System.out.print("1. println");
+        System.out.print("2. println");
+
+        System.out.println();
+
+        // System.out.printf(format, argument);
+        System.out.printf("Java is %d percent fun!", 100);
+
+        System.out.println();
+
+        System.out.println(10);
+        System.out.println("Bharat");
+        System.out.println("Bharat " + "Kumar");
+    }
+}
