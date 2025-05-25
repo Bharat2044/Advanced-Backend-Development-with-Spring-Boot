9cc8aa94b244833fc8edeaf165739720f9b0ce04
Bharat Kumar <bk9955204456@gmail.com>
2025-05-25 16:03:28 +0530

    code added

---
+++ 001. Basics of JAVA Programming Language/src/_03_Conditionals_in_JAVA/_06_CheckEvenOdd.java
@@ -0,0 +1,20 @@
+package _03_Conditionals_in_JAVA;
+
+import java.util.Scanner;
+
+public class _06_CheckEvenOdd {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+
+        if (num % 2 == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
