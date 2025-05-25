03ec0cc1b2eae8ad97f345e6071b7fc2038ed434
Bharat Kumar <bk9955204456@gmail.com>
2025-05-25 16:03:12 +0530

    code added

---
+++ 001. Basics of JAVA Programming Language/src/_02_Scanner_class_and_Operators_in_JAVA/_06_UnaryOperators.java
@@ -0,0 +1,35 @@
+package _02_Scanner_class_and_Operators_in_JAVA;
+
+public class _06_UnaryOperators {
+
+    public static void main(String[] args) {
+
+        int num = 5;
+        System.out.println(num++);
+        System.out.println(++num);
+        System.out.println(num--);
+        System.out.println(--num);
+
+        int a = 12, b = 12;
+        int res1, res2;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        res1 = ++a;
+        System.out.println("res1 = " + res1);
+        System.out.println("a = " + a);
+
+        res2 = --b;
+        System.out.println("res2 = " + res2);
+        System.out.println("b = " + b);
+
+        res1 = a++;
+        System.out.println("res1 = " + res1);
+        System.out.println("a = " + a);
+
+        res2 = b--;
+        System.out.println("res2 = " + res2);
+        System.out.println("b = " + b);
+    }
+}
---
+++ 001. Basics of JAVA Programming Language/src/_03_Conditionals_in_JAVA/_02_IfElseBlock.java
@@ -0,0 +1,15 @@
+package _03_Conditionals_in_JAVA;
+
+public class _02_IfElseBlock {
+
+    public static void main(String[] args) {
+
+        boolean primeSubs = true;
+
+        if (primeSubs == true) {
+            System.out.println("Welcome to the Prime Subscription");
+        } else {
+            System.out.println("Please become a member of amazon Prime now.");
+        }
+    }
+}
