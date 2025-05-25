fedf7b0f70270b3e730abf4ef8a56a10e0c4b9c9
Bharat Kumar <bk9955204456@gmail.com>
2025-05-25 16:03:24 +0530

    code added

---
+++ 001. Basics of JAVA Programming Language/src/_03_Conditionals_in_JAVA/_01_IfBlock.java
@@ -0,0 +1,19 @@
+package _03_Conditionals_in_JAVA;
+
+public class _01_IfBlock {
+
+    public static void main(String[] args) {
+
+        int num = 10;
+
+        if (num > 0) {
+            System.out.println("It is a positive number");
+        }
+
+        if (num < 0) {
+            System.out.println("It is a negative number");
+        }
+
+        System.out.println("Now you are outside of if block");
+    }
+}
---
+++ 001. Basics of JAVA Programming Language/src/_03_Conditionals_in_JAVA/_05_SwitchEnhanced.java
@@ -0,0 +1,18 @@
+package _03_Conditionals_in_JAVA;
+
+public class _05_SwitchEnhanced {
+    public static void main(String[] args) {
+
+        int number = 48;
+
+        String size = switch (number) {
+            case 29 -> "Small";
+            case 42 -> "Medium";
+            case 48 -> "Large";
+            case 50 -> "Extra Large";
+            default -> "Unknown";
+        };
+
+        System.out.println("Size = " + size);
+    }
+}
