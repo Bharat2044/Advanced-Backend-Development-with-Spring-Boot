d0d6b0528db20d8311dbc4aac57a1819e7346011
Bharat Kumar <bk9955204456@gmail.com>
2025-05-25 16:03:17 +0530

    code added

---
+++ 001. Basics of JAVA Programming Language/src/_03_Conditionals_in_JAVA/_03_IfElseIfBlock.java
@@ -0,0 +1,19 @@
+package _03_Conditionals_in_JAVA;
+
+public class _03_IfElseIfBlock {
+
+    public static void main(String[] args) {
+
+        char hotstarSubs = 'S';
+
+        if (hotstarSubs == 'N') {
+            System.out.println("You don't have any Subscription");
+        } else if (hotstarSubs == 'S') {
+            System.out.println("You have super Subscription");
+        } else if (hotstarSubs == 'P') {
+            System.out.println("You have premium Subscription");
+        } else {
+            System.out.println("This looks like an invalid character");
+        }
+    }
+}
