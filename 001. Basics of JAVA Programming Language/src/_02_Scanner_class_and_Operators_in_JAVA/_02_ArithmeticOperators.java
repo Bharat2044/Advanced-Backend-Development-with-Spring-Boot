1f3456bbb3f14779839eb63bfe54194f99f7d3fd
Bharat Kumar <bk9955204456@gmail.com>
2025-05-25 16:03:07 +0530

    code added

---
+++ 001. Basics of JAVA Programming Language/src/_02_Scanner_class_and_Operators_in_JAVA/_02_ArithmeticOperators.java
@@ -0,0 +1,24 @@
+package _02_Scanner_class_and_Operators_in_JAVA;
+
+public class _02_ArithmeticOperators {
+
+    public static void main(String[] args) {
+
+        int a = 12, b = 5;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
+        System.out.println(a % b);
+
+        System.out.println(5.0 / 2);
+        System.out.println(5.0 % 2);
+
+        System.out.println("Sum = " + (a + b));
+        System.out.println("Difference = " + (a - b));
+        System.out.println("Product = " + (a * b));
+        System.out.println("Division = " + (a / b));
+        System.out.println("Remainder = " + (a % b));
+    }
+}
---
+++ 001. Basics of JAVA Programming Language/src/_02_Scanner_class_and_Operators_in_JAVA/_07_BitwiseOperators.java
@@ -0,0 +1,17 @@
+package _02_Scanner_class_and_Operators_in_JAVA;
+
+public class _07_BitwiseOperators {
+
+    public static void main(String[] args) {
+
+        int a = 5, b = 7;
+
+        System.out.println(a & b);
+        System.out.println(a | b);
+        System.out.println(a ^ b);
+        System.out.println(~a);
+        System.out.println(10 >> 2);
+        System.out.println(10 >>> 2);
+        System.out.println(10 << 2);
+    }
+}
