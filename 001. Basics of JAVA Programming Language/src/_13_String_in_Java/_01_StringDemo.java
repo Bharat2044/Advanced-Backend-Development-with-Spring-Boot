f0b6d2b5115bd232384b411b64649e893d39bdef
Bharat Kumar <bk9955204456@gmail.com>
2025-06-11 01:38:32 +0530

    code added

---
+++ 001. Basics of JAVA Programming Language/src/_13_String_in_Java/_01_StringDemo.java
@@ -0,0 +1,18 @@
+package _13_String_in_Java;
+
+public class _01_StringDemo {
+
+    public static void main(String[] args) {
+
+        String first = "Java";
+        String second = "Python";
+        String third = "JavaScript";
+
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+
+        String name = new String("Bharat");
+        System.out.println(name);
+    }
+}
