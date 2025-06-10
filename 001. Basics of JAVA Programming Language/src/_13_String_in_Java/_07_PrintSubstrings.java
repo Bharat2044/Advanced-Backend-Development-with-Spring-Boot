c1bc728df38e3b0cf55704df288569823632fe19
Bharat Kumar <bk9955204456@gmail.com>
2025-06-11 01:39:16 +0530

    code added

---
+++ 001. Basics of JAVA Programming Language/src/_13_String_in_Java/_07_PrintSubstrings.java
@@ -0,0 +1,12 @@
+package _13_String_in_Java;
+
+public class _07_PrintSubstrings {
+
+    public static void main(String[] args) {
+
+        String str = "Hello World";
+
+        System.out.println(str.substring(1));
+        System.out.println(str.substring(1, 7));
+    }
+}
