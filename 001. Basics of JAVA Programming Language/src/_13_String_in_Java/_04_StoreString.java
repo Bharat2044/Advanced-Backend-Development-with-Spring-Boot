855e1fb9d246960e6357b2a8e5ad67bf3a7b5c65
Bharat Kumar <bk9955204456@gmail.com>
2025-06-11 01:38:49 +0530

    code added

---
+++ 001. Basics of JAVA Programming Language/src/_13_String_in_Java/_04_StoreString.java
@@ -0,0 +1,10 @@
+package _13_String_in_Java;
+
+public class _04_StoreString {
+
+    public static void main(String[] args) {
+
+        String example = "This is a \"very\" important project";
+        System.out.println(example);
+    }
+}
