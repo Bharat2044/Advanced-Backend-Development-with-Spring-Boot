d543d0b14fb2df6e485a63c2a15c2aba8cbdb139
Bharat Kumar <bk9955204456@gmail.com>
2025-06-11 13:26:06 +0530

    code added

---
+++ 001. Basics of JAVA Programming Language/src/_17_Stack_Memory_and_Heap_Memory/_01_StackMemoryDemo1.java
@@ -0,0 +1,20 @@
+package _17_Stack_Memory_and_Heap_Memory;
+
+public class _01_StackMemoryDemo1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hi There");
+        
+        greeting();
+        askQuery();
+    }
+
+    private static void askQuery() {
+        System.out.println("Hope! you're doing well");
+    }
+
+    private static void greeting() {
+        System.out.println("How may I help you?");
+    }
+}
