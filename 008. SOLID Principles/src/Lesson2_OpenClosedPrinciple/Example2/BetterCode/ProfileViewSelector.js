bb12798c3ff70c10644ce708b97af91504808be0
Bharat Kumar <bk9955204456@gmail.com>
2025-06-20 21:16:04 +0530

    code added

---
+++ 008. SOLID Principles/src/Lesson2_OpenClosedPrinciple/Example2/BetterCode/ProfileViewSelector.js
@@ -0,0 +1,15 @@
+const ProfileViewSelector = {
+
+    getProfileView(role) {
+        switch (role) {
+            case 'admin' :
+                return <AdminView/>
+            case 'editor':
+                return <EditorView/>
+            default:
+                return <StardView/>
+        }
+    }
+}
+
+export default ProfileViewSelector;
---
+++ 008. SOLID Principles/src/Lesson2_OpenClosedPrinciple/Example2/BetterCode/UserProfile.js
@@ -0,0 +1,10 @@
+import ProfileViewSelector from "./ProfileViewSelector";
+
+const UserProfile = ({user}) => {
+
+    const ProfileView = ProfileViewSelector.getProfileView(user.role);
+
+    return <ProfileView user={user}/>
+}
+
+export default UserProfile;
