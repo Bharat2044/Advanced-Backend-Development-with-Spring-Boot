2bf5334851627c8457d183bd5f2a784c1d214f65
Bharat Kumar <bk9955204456@gmail.com>
2025-06-21 13:49:37 +0530

    code added

---
+++ 008. SOLID Principles/src/Lesson4_InterfaceSegregationPrinciple/BetterCode/Seller.java
@@ -0,0 +1,19 @@
+package Lesson4_InterfaceSegregationPrinciple.BetterCode;
+
+public class Seller implements ICanModify, ICanBuy, ICanSell {
+
+    @Override
+    public void buy() {
+
+    }
+
+    @Override
+    public void modify() {
+
+    }
+
+    @Override
+    public void sell() {
+
+    }
+}
