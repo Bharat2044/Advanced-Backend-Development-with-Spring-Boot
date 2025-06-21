7618772aead0f1962b0febd969883ad763eba0b7
Bharat Kumar <bk9955204456@gmail.com>
2025-06-21 13:50:34 +0530

    code added

---
+++ 008. SOLID Principles/src/Lesson5_DependencyInversionPrinciple/ProblematicCode/PostgreSQLDatabase.java
@@ -0,0 +1,8 @@
+package Lesson5_DependencyInversionPrinciple.ProblematicCode;
+
+public class PostgreSQLDatabase {
+
+    public void connect() {
+        System.out.println("Connected to PostgreSQL Database");
+    }
+}
