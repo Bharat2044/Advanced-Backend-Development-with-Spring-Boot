43e919a9a3cbbe04ef885ec3d179a09983ec264b
Bharat Kumar <bk9955204456@gmail.com>
2025-06-21 13:50:26 +0530

    code added

---
+++ 008. SOLID Principles/src/Lesson5_DependencyInversionPrinciple/ProblematicCode/MySQLDatabase.java
@@ -0,0 +1,8 @@
+package Lesson5_DependencyInversionPrinciple.ProblematicCode;
+
+public class MySQLDatabase {
+
+    public void connect() {
+        System.out.println("Connected to MySQL Database");
+    }
+}
