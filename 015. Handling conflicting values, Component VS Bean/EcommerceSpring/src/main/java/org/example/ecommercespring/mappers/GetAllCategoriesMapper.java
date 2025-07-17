d31b169deabcda545551da8e53e0049a588c8809
Bharat Kumar <bk9955204456@gmail.com>
2025-07-17 13:44:38 +0530

    code added

---
+++ 015. Handling conflicting values, Component VS Bean/EcommerceSpring/src/main/java/org/example/ecommercespring/mappers/GetAllCategoriesMapper.java
@@ -0,0 +1,22 @@
+package org.example.ecommercespring.mappers;
+
+import org.example.ecommercespring.dto.CategoryDTO;
+import org.example.ecommercespring.dto.FakeStoreCategoryResponseDTO;
+
+import java.util.List;
+
+public class GetAllCategoriesMapper {
+
+    public static FakeStoreCategoryResponseDTO toFakeStoreCategoryDTO() {
+        return null;
+    }
+
+    public static List<CategoryDTO> toCategoryDTO(FakeStoreCategoryResponseDTO dto) {
+
+        return dto.getCategories().stream()
+                .map(category -> CategoryDTO.builder()
+                        .name(category)
+                        .build())
+                .toList();
+    }
+}
