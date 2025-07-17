cbbdcc71fa16dbdcd5f7328f1db3dfd81dfe20b1
Bharat Kumar <bk9955204456@gmail.com>
2025-07-17 13:44:30 +0530

    code added

--- 015. Handling conflicting values, Component VS Bean/EcommerceSpring/src/main/java/org/example/ecommercespring/gateway/FakeStoreRestTemplateGateway.java
+++ 015. Handling conflicting values, Component VS Bean/EcommerceSpring/src/main/java/org/example/ecommercespring/gateway/FakeStoreRestTemplateGateway.java
@@ -1,18 +1,47 @@
 package org.example.ecommercespring.gateway;
 
 import org.example.ecommercespring.dto.CategoryDTO;
+import org.example.ecommercespring.dto.FakeStoreCategoryResponseDTO;
+import org.example.ecommercespring.mappers.GetAllCategoriesMapper;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.util.List;
+
+import static org.example.ecommercespring.mappers.GetAllCategoriesMapper.toCategoryDTO;
 
 @Component("fakeStoreRestTemplateGateway")
 //@Primary
 public class FakeStoreRestTemplateGateway implements ICategoryGateway {
 
+    private final RestTemplateBuilder restTemplateBuilder;
+
+    public FakeStoreRestTemplateGateway(RestTemplateBuilder restTemplateBuilder) {
+        this.restTemplateBuilder = restTemplateBuilder;
+    }
+
     @Override
     public List<CategoryDTO> getAllCategories() throws IOException {
-        return List.of();
+
+        RestTemplate restTemplate = restTemplateBuilder.build();
+        String url = "https://fakestoreapi.in/api/products/category";
+
+        ResponseEntity<FakeStoreCategoryResponseDTO> response = restTemplate.getForEntity(url, FakeStoreCategoryResponseDTO.class);
+
+        if (response.getBody() == null) {
+            throw new IOException("Failed to fetch categories from FakeStore API");
+        }
+
+//        return response.getBody().getCategories().stream()
+//                .map(category -> CategoryDTO.builder()
+//                        .name(category)
+//                        .build())
+//                .toList();
+
+        return GetAllCategoriesMapper.toCategoryDTO(response.getBody());
     }
 }
